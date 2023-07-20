923b54758380e40cd4a8abd542878a73cc2aea5a
Christian Jones <chjones2015@gmail.com>
2023-07-19 19:21:21 -0500

    added characters and function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment code here
+var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
+function generatePassword() {
+  var passwordLength = 0;
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
