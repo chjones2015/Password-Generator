3120e1c055e2ee4054ca571af1e67e215e3b96cf
Christian Jones <chjones2015@gmail.com>
2023-07-19 19:35:37 -0500

    created arrays for all types of characters

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+var lowercaseChars = [];
+var uppercaseChars = [];
+var numericChars = [];
+var specialChars = [];
+
+for (var i = 0; i < characters.length; i++) {
+  var char = characters[i];
+  if (char >= "a" && char <= "z") {
+    lowercaseChars.push(char);
+  } else if (char >= "A" && char <= "Z") {
+    uppercaseChars.push(char);
+  } else if (char >= "0" && char <= "9") {
+    numericChars.push(char);
+  } else {
+    specialChars.push(char);
+  }
+}
 
 function generatePassword() {
   var passwordLength = 0;
